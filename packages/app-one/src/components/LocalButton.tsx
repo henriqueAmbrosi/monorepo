8cd8549f3bcf840759fb100f5a03d6e6b2aa7482
HenriqueAmbrosi2002 <henrique@infoservsoftware.com.br>
2021-01-26 08:41:09 -0300

    teste

--- packages/app-one/src/components/LocalButton.tsx
+++ packages/app-one/src/components/LocalButton.tsx
@@ -3,5 +3,5 @@ interface Props {
 }
 
 export default function LocalButton(props: Props): JSX.Element {
-  return <button data-testid="local-button">{props.label}</button>;
+  return <button data-testid="local-button">{props.label} {teste}</button>;
 }
