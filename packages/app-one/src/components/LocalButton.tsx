795660db1778b696e85fc18d4eecec0fc791c5c9
henriqueAmbrosi <54010490+henriqueAmbrosi@users.noreply.github.com>
2021-02-02 11:12:39 -0300

    Update LocalButton.tsx

--- packages/app-one/src/components/LocalButton.tsx
+++ packages/app-one/src/components/LocalButton.tsx
@@ -3,5 +3,5 @@ interface Props {
 }
 
 export default function LocalButton(props: Props): JSX.Element {
-  return <button data-testid="local-button">{props.label} sa</button>;
+  return <button data-testid="local-button">{props.label} s</button>;
 }
